c7b4548e6ad42b351183a812c0ac90714ea3fda6
Madusha Navodini <madushanavodini0924@gmail.com>
2026-07-03 17:58:01 +0530

    Change Book to BookManual in main method

--- LibraryManual.java
+++ LibraryManual.java
@@ -1,6 +1,6 @@
 public class LibraryManual {
     public static void main(String[] args){
-        Book B1 = new Book("S234", "SherlockHolmes", "ArthurConanDoyle", "Detective Fiction", "Yes");
+        BookManual B1 = new BookManual("S234", "SherlockHolmes", "ArthurConanDoyle", "Detective Fiction", "Yes");
         B1.borrowBook();
         B1.DisplayBookDetails();
     }
