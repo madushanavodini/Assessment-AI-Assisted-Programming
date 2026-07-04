5dc916285b86158976dd67da2035547d3dce58c0
madushanavodini <madushanavodini0924@gmail.com>
2026-07-04 20:40:50 +0530

    Fixed project structure and updated LibraryManual

---
+++ LibraryManual.java
@@ -0,0 +1,8 @@
+public class LibraryManual {
+    public static void main(String[] args){
+        BookManual B1 = new BookManual("S234", "SherlockHolmes", "ArthurConanDoyle", "Detective Fiction", "Yes");
+        B1.markasborrowBook();
+        B1.DisplayBookDetails();
+    }
+
+}
