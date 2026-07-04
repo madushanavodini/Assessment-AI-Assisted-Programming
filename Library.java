252a01cbf1bda2e684c4de2c2d8cdea4e034dd0b
madushanavodini <madushanavodini0924@gmail.com>
2026-07-04 20:45:13 +0530

    Remove Library.java

--- Library.java
+++
@@ -1,8 +0,0 @@
-public class Library {
-    public static void main(String[] args){
-        Book B1 = new Book("S234", "SherlockHolmes", "ArthurConanDoyle", "Detective Fiction", "Yes");
-        B1.borrowBook();
-        B1.DisplayBookDetails();
-    }
-
-}
