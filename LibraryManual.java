12a571b30f783aefa2cafda37c43d41caa73db8b
Madusha Navodini <madushanavodini0924@gmail.com>
2026-07-05 21:04:40 +0530

    Change borrowBook to markasborrowBook method

--- LibraryManual.java
+++ LibraryManual.java
@@ -1,7 +1,7 @@
 public class LibraryManual {
     public static void main(String[] args){
         BookManual B1 = new BookManual("S234", "SherlockHolmes", "ArthurConanDoyle", "Detective Fiction", "Yes");
-        B1.borrowBook();
+        B1.markasborrowBook();
         B1.DisplayBookDetails();
     }
 
