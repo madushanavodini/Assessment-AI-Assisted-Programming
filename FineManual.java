0438b4a3b1bc152e02959363a36129736d194dac
Madusha Navodini <madushanavodini0924@gmail.com>
2026-07-11 11:58:35 +0530

    Delete FineManual.java

--- FineManual.java
+++
@@ -1,7 +0,0 @@
-class Fine {
-    private static final double RATE_PER_DAY = 10.0;
-
-    public static double calculate(TransactionManual t) {
-        return t.getDaysOverdue() * RATE_PER_DAY;
-    }
-}
